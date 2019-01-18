52a9108959644bdaa0704416b62bb3d1f6b40258
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-01-18 21:47:05 +0800

    Parser completed. Now write some tests for it.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,20 +1,64 @@
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Parser {
-    public Parser(String path) {
+    private Scanner scanner;
+    private CommandType commandType;
+    private Map<Integer, String> command;
 
+    public Parser(String path) {
+        scanner = new Scanner(path);
     }
 
     public boolean hasMoreCommands() {
-        return false;
+        return scanner.hasNextLine();
     }
 
     public void advance() {
+        /*  Read one line.
+            Remove the comments and blanks.
+                if "", set command type and command to null.
+            set the command type
+            set the command
+         */
+        String s = scanner.nextLine();
+        s = removeComment(s);
+        commandType = setCommandType(s);
+        command = setCommand(s);
+    }
 
+    private Map<Integer, String> setCommand(String s) {
+        Map<Integer, String> map = new HashMap<>();
+        String[] splited = s.split("\\s+");
+        for (int i = 0; i < splited.length; i++) {
+            map.put(i, splited[i]);
+        }
+        return map;
+    }
+
+    private CommandType setCommandType(String s) {
+        if (s.equals("")) {
+            return null;
+        }
+        String[] splited = s.split("\\s+");
+        if (splited.length == 1) {
+            return CommandType.C_ARITHMETIC;
+        }
+        if (splited.length == 3) {
+            if (splited[0].equals("pop")) {
+                return CommandType.C_POP;
+            }
+            if (splited[0].equals("push")) {
+                return CommandType.C_PUSH;
+            }
+        }
+        // More coming.
+        return null;
     }
 
     public CommandType getCommandType() {
-        return null;
+        return commandType;
     }
 
     /** Each command is represented by a map, where key is the index, value is the string
@@ -22,6 +66,15 @@ public class Parser {
      * @return the map
      */
     public Map<Integer, String> getCommand() {
-        return null;
+        return command;
+    }
+
+    private static String removeComment(String s) {
+        // Remove the comments.
+        int offSet = s.indexOf("//");
+        if (offSet != -1) {
+            s = s.substring(0, offSet);
+        }
+        return s;
     }
 }
--- src/main/java/VMTranslator.java
+++ src/main/java/VMTranslator.java
@@ -3,11 +3,12 @@ public class VMTranslator {
         if (args.length != 1) {
             throw new IllegalArgumentException("You should only provide a path.");
         }
-        if (!args[0].split("\\.")[1].equals("vm")) {
+        String path = args[0];
+        if (!path.split("\\.")[1].equals("vm")) {
             throw new IllegalArgumentException("Must specify a valid .vm file!");
         }
-        Parser parser = new Parser(args[0]);
-        String outFilePath = args[0].split("\\.")[0] + ".asm";
+        Parser parser = new Parser(path);
+        String outFilePath = path.split("\\.")[0] + ".asm";
         CodeWriter codeWriter = new CodeWriter(outFilePath);
         while (parser.hasMoreCommands()) {
             parser.advance();
