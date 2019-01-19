adc4f2d4f52807b18772467e50adf20e65c924f4
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-01-19 11:37:04 +0800

    Skeleton for writer.

--- src/main/java/CodeWriter.java
+++ src/main/java/CodeWriter.java
@@ -13,11 +13,27 @@ public class CodeWriter {
     }
 
     public void writeArithmetic(Map<Integer, String> command) {
+        String comment = generateComment(command);
+        String assembly = generateArithmetic(command);
+        printStream.println(comment + assembly);
+    }
 
+    private String generateArithmetic(Map<Integer, String> command) {
+        return "this is generateArithmetic";
     }
 
     public void writePushPop(Map<Integer, String> command) {
+        String comment = generateComment(command);
+        String assembly = generatePushPop(command);
+        printStream.println(comment + assembly);
+    }
 
+    private String generateComment(Map<Integer, String> command) {
+        return "// this is generateComment" + System.lineSeparator();
+    }
+
+    private String generatePushPop(Map<Integer, String> command) {
+        return "this is generatePushPop";
     }
 
     public void close() {
